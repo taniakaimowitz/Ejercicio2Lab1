d89c0213e4b5e034867d9a6ce63db8b02cb575e1
taniakaimowitz <33156813+taniakaimowitz@users.noreply.github.com>
2017-10-27 13:40:26 -0600

    Add files via upload

---
+++ Laboratorio2.py
@@ -0,0 +1,15 @@
+
+def sum_items(lista):
+    suma = 0
+    for x in lista:
+        if type(x) is int:
+            suma = suma + x
+    if suma == 0:
+        print ("Ningun ítem es numérico")
+    print ('La suma de todos los ítems es ' + str(suma))
+sum_items([“box”, “car”, “cat”, “tree”])
+
+
+        
+       
+
